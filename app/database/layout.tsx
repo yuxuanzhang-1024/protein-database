c9282182933263f83df00e1fe2301d6083db0393
yuxuan <yzhangms@connect.ust.hk>
2024-10-14 19:17:39 +0800

    test

--- app/database/layout.tsx
+++ app/database/layout.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { Header } from "../ui/header";
-export const experimental_ppr = true;
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <div>
